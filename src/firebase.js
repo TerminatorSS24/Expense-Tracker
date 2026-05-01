bcef851a3756ef67d0936e315c3a499205a29104
TerminatorSS24 <subhraneelsaha9674587481@gmail.com>
2026-05-01 15:29:18 +0530

    Env addition

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyButZZ5BroDEyaVmeW_r-_RqE_ymx8hCN8",
-  authDomain: "expense-tracker-6072c.firebaseapp.com",
-  projectId: "expense-tracker-6072c",
-  storageBucket: "expense-tracker-6072c.firebasestorage.app",
-  messagingSenderId: "804648186309",
-  appId: "1:804648186309:web:945229122eedb3846c5bac",
-  measurementId: "G-10SSDTQ5GZ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
